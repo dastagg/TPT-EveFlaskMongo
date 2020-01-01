19cc5aa735b43ffb019282bd4197a7f9181e75dc
dastagg(opus3) <dastagg@stagglinux.com>
2020-01-01 10:01:15 -0500

    End of Validation chapter

--- settings.py
+++ settings.py
@@ -7,7 +7,34 @@ RESOURCE_METHODS = ["GET", "POST", "DELETE"]
 ITEM_METHODS = ["GET", "PATCH", "PUT", "DELETE"]
 
 # domain definition
-people_schema = {"firstname": {"type": "string"}, "lastname": {"type": "string"}}
+people_schema = {
+    "firstname": {"type": "string", "minlength": 1, "maxlength": 30,},
+    "lastname": {"type": "string", "maxlength": 50, "required": True, "unique": True,},
+    "middle_name": {"dependencies": ["firstname", "lastname"]},
+    "born": {"type": "datetime"},
+    "age": {
+        "type": "integer",
+        "isodd": True,
+        # "coerce": int
+    },
+    "role": {
+        "type": "list",
+        "allowed": ["author", "contributor", "copy"],
+        "default": ["author"],
+    },
+    "location": {
+        "type": "dict",
+        "schema": {
+            "address": {"type": "string"},
+            "city": {"type": "string", "required": True},
+        },
+    },
+    "email": {"type": "email"},
+    "prop1": {
+        "anyof_type": ["integer", "float"],
+        "anyof": [{"min": 0, "max": 10}, {"min": 100, "max": 110}],
+    },
+}
 
 DOMAIN = {
     "people": {"schema": people_schema},
