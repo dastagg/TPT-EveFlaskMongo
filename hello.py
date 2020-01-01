cc2fd04700199688ee24143fe29c11b86cbe0797
dastagg(opus3) <dastagg@stagglinux.com>
2019-12-31 19:12:35 -0500

    Intro to Flask

---
+++ hello.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return jsonify({"message": "Hello World"})
+
+
+@app.route("/login")
+def login():
+    return "Hey you need to login"
